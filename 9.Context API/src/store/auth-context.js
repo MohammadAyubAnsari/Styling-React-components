ed2f10ea235f8c58161e217a7b1c6d032e355d2e
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2023-03-14 19:58:00 +0530

    9.Context API

---
+++ 9.Context API/src/store/auth-context.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// const AuthContext = React.createContext({
+//   isLoggedIn: false
+// });
+
+// export default AuthContext;
+
+import React from "react";
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+});
+
+export default AuthContext;
